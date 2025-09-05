0b4c8d07debbedc7e4a8ac75673b1a9f63d878a8
ssougata-doolai <sougatadolai00@gmail.com>
2025-09-05 12:59:24 +0530

    Time: 15 ms (9.61%) | Memory: 22 MB (15.31%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -25,31 +25,4 @@ class Solution:
         new_list.append([prev0, prev1])
 
         return new_list
-        
-        # new_list = []
-        # l = len(intervals)
-        # if l <= 1:
-        #     return intervals
-
-        # prev0 = intervals[0][0]
-        # prev1 = intervals[0][1]
-        # for i in range(1, l):
-        #     next0 = intervals[i][0]
-        #     next1 = intervals[i][1]
-
-        #     # case 1: [5, 6], [4, 7]
-        #     if prev0 >= next0 and prev1 <= next1:
-        #         print("1")
-        #         prev0 = next0
-        #         prev1 = next1
-
-        #     # case 2: reverse [4, 7], [5, 6]
-        #     elif prev0 <= next0 and prev1 >= next1:
-        #         print("2")
-
-        #     # case 3: overlapping
-        #     elif prev1 >= next0 and prev1 <= next1:
-        
-        # new_list.append([prev0, prev1])
-        # return new_list
                 
