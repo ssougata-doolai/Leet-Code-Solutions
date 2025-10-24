f39044d77612d41553e94f8caea9b377057cef72
ssougata-doolai <sougatadolai00@gmail.com>
2025-10-24 20:17:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (56.63%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.py
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.py
@@ -1,31 +1,38 @@
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        def binary_search(arr, k):
-            min_n = 0
-            max_n = len(arr) - 1
-            while min_n <= max_n:
-                mid = (min_n + max_n) // 2
-                if arr[mid] == k:
-                    return True
-                elif k < arr[mid]:
-                    max_n = mid - 1
-                else:
-                    min_n = mid + 1
+        for i in range(len(arr)):
+            if arr[i] <= k:
+                k += 1
+            else:
+                break
+        return k
 
-            return False 
+        # def binary_search(arr, k):
+        #     min_n = 0
+        #     max_n = len(arr) - 1
+        #     while min_n <= max_n:
+        #         mid = (min_n + max_n) // 2
+        #         if arr[mid] == k:
+        #             return True
+        #         elif k < arr[mid]:
+        #             max_n = mid - 1
+        #         else:
+        #             min_n = mid + 1
 
-        def linear_search(arr, k):
-            for i in range(len(arr)):
-                if arr[i] == k:
-                    return True
+        #     return False 
+
+        # def linear_search(arr, k):
+        #     for i in range(len(arr)):
+        #         if arr[i] == k:
+        #             return True
                 
-            return False
+        #     return False
         
-        num = 0
-        count = 0
-        while count < k:
-            num += 1
-            if binary_search(arr, num) == False:
-                count += 1
+        # num = 0
+        # count = 0
+        # while count < k:
+        #     num += 1
+        #     if binary_search(arr, num) == False:
+        #         count += 1
             
-        return num
+        # return num
