b3bed3a2560130d524f3e677cb3814bbfaec3665
ssougata-doolai <sougatadolai00@gmail.com>
2025-10-26 19:29:12 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (92.46%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,27 +1,41 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def is_valid(res):
-            bal = 0
-            for c in res:
-                if c == "(":
-                    bal += 1
-                else:
-                    bal -= 1
-                if bal < 0:
-                    return False
-            return bal == 0
-
-        def generate_all(p, n, res):
-            if len(p) == n*2:
-                if is_valid(p):
-                    res.append(p)
+        def generate(curr, op, cl, n, res):
+            if len(curr) == n*2:
+                res.append(curr)
                 return
-            generate_all(p + "(", n, res)
-            generate_all(p + ")", n, res)
+            if op < n:
+                generate(curr + "(", op+1, cl, n, res)
+            if cl < op:
+                generate(curr + ")", op, cl+1, n, res)
         
-        def generate(n):
-            res = []
-            generate_all("", n, res)
-            return res
+        res = []
+        generate("", 0, 0, n, res)
+        return res
 
-        return generate(n)
+        # Brute Force
+        # def is_valid(res):
+        #     bal = 0
+        #     for c in res:
+        #         if c == "(":
+        #             bal += 1
+        #         else:
+        #             bal -= 1
+        #         if bal < 0:
+        #             return False
+        #     return bal == 0
+
+        # def generate_all(p, n, res):
+        #     if len(p) == n*2:
+        #         if is_valid(p):
+        #             res.append(p)
+        #         return
+        #     generate_all(p + "(", n, res)
+        #     generate_all(p + ")", n, res)
+        
+        # def generate(n):
+        #     res = []
+        #     generate_all("", n, res)
+        #     return res
+
+        # return generate(n)
