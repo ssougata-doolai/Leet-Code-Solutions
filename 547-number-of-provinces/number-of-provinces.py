f34779e5844fbf11d5db23b9b9678de318ebb33f
ssougata-doolai <sougatadolai00@gmail.com>
2025-12-04 18:38:36 +0530

    Time: 4 ms (72.16%) | Memory: 19.1 MB (45.67%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,21 +1,23 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        from collections import deque
         n = len(isConnected)
         if n == 0:
             return 0
+
+        from collections import deque
         visited = [False] * n
         count = 0
+
         def bfs(i):
             queue = deque([i])
             visited[i] = True
 
             while queue:
                 node = queue.popleft()
-                for j in range(n):
-                    if isConnected[node][j] == 1 and not visited[j]:
-                        visited[j] = True
-                        queue.append(j)
+                for neighbour in range(n):
+                    if isConnected[node][neighbour] == 1 and not visited[neighbour]:
+                        queue.append(neighbour)
+                        visited[neighbour] = True
 
         for i in range(n):
             if visited[i] == False:
@@ -23,3 +25,26 @@ class Solution:
                 count += 1
         
         return count
+
+        # n = len(isConnected)
+        # if n == 0:
+        #     return 0
+        # visited = [False] * n
+        # count = 0
+        # def bfs(i):
+        #     queue = deque([i])
+        #     visited[i] = True
+
+        #     while queue:
+        #         node = queue.popleft()
+        #         for j in range(n):
+        #             if isConnected[node][j] == 1 and not visited[j]:
+        #                 visited[j] = True
+        #                 queue.append(j)
+
+        # for i in range(n):
+        #     if visited[i] == False:
+        #         bfs(i)
+        #         count += 1
+        
+        # return count
