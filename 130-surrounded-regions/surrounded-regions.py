3a90d8832a9b9f9123c4a1ae79e999cae3c534db
ssougata-doolai <sougatadolai00@gmail.com>
2025-12-11 18:36:52 +0530

    Time: 3 ms (86.32%) | Memory: 21.7 MB (56.20%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -4,7 +4,7 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         from collections import deque
-        l = []
+        l = set()
         r, c = len(board), len(board[0])
         queue = deque()
 
@@ -12,7 +12,7 @@ class Solution:
             for j in range(c):
                 if (i == 0 or j == 0 or i == r-1 or j == c-1) and board[i][j] == "O":
                     queue.append((i, j))
-                    l.append((i, j))
+                    l.add((i, j))
         
         directions = [(-1, 0), (0, +1), (+1, 0), (0, -1)]
         while queue:
@@ -21,7 +21,7 @@ class Solution:
                 nr, nc = i+x, j+y
                 if 0 <= nr < r and 0 <= nc < c and board[nr][nc] == "O" and (nr, nc) not in l:
                     queue.append((nr, nc))
-                    l.append((nr, nc))
+                    l.add((nr, nc))
 
         for i in range(r):
             for j in range(c):
