6e6007cbf03d4a3ffc5e5f4b273dd2b42db714e3
ssougata-doolai <sougatadolai00@gmail.com>
2026-01-01 22:18:39 +0530

    Time: 932 ms (17.46%) | Memory: 20.7 MB (59.40%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -25,12 +25,12 @@ class Solution:
                     k -= 1
 
                 if target < 0:
-                    # while j < k and nums[j] == nums[j+1]:
-                    #     j += 1
+                    while j < k and nums[j] == nums[j+1]:
+                        j += 1
                     j += 1
 
                 if target > 0:
-                    # while j < k and nums[k] == nums[k-1]:
-                    #     k -= 1
+                    while j < k and nums[k] == nums[k-1]:
+                        k -= 1
                     k -= 1
         return result
