7d8ccdea693088a1e630e771701e067c07a2c08b
ssougata-doolai <sougatadolai00@gmail.com>
2026-01-01 22:12:04 +0530

    Time: 937 ms (17.35%) | Memory: 20 MB (93.97%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,31 +1,36 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-        res = []
         nums.sort()
+        result = []
         n = len(nums)
-        for i in range(n):
+
+        for i in range(n-2):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            
-            target = - nums[i]
-            left, right = i+1, n-1
-            while left < right:
-                s = nums[left] + nums[right]
-                if s == target:
-                    res.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
 
-                    left += 1
-                    right -= 1
+            j = i + 1
+            k = n - 1
 
-                elif s < target:
-                    left += 1
-                else: 
-                    right -= 1
-        
-        return res
-        
+            while j < k:
+                target = nums[i] + nums[j] + nums[k]
+                if target == 0:
+                    result.append([nums[i], nums[j], nums[k]])
+
+                    while j < k and nums[j] == nums[j+1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k-1]:
+                        k -= 1
+
+                    j += 1
+                    k -= 1
+
+                if target < 0:
+                    while j < k and nums[j] == nums[j+1]:
+                        j += 1
+                    j += 1
+
+                if target > 0:
+                    while j < k and nums[k] == nums[k-1]:
+                        k -= 1
+                    k -= 1
+        return result
