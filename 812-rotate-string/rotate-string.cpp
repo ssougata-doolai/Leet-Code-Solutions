e0ad308850017c6d87c7694df444549dd9e8151a
ssougata-doolai <sougatadolai00@gmail.com>
2026-02-05 19:53:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (37.11%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        for(int i = 0; i < s.size(); i++) {
-            if(s.substr(i) + s.substr(0, i) == goal) return true;
-        }
-        return false;
+        // for(int i = 0; i < s.size(); i++) {
+        //     if(s.substr(i) + s.substr(0, i) == goal) return true;
+        // }
+        // return false;
+        if(s.size() != goal.size()) return false;
+        
+        string ss = s+s;
+        return ss.find(goal) != string::npos;
     }
 };
