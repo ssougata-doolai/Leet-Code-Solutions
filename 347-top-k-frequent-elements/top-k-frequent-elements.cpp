3fb2540b71c27cf70ddda77ea248d02ce7dfa968
ssougata-doolai <sougatadolai00@gmail.com>
2026-02-10 17:45:53 +0530

    Time: 4 ms (35.19%) | Memory: 18 MB (62.69%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> count;
+        // unordered_map<int, int> count;
+        // for(int &n : nums) {
+        //     count[n]++;
+        // }
+
+        // vector<pair<int, int>> freq;
+        // for(auto &c: count) {
+        //     freq.push_back({c.second, c.first});
+        // }
+
+        // sort(freq.begin(), freq.end());
+
+        // int indx = 0;
+        // vector<int> res;
+
+        // for(int i = freq.size()-1; i >=0 ; i--) {
+        //     res.push_back(freq[i].second);
+        //     if(++indx == k) break;
+        // }
+        // return res;
+
+        unordered_map<int, int> count; 
+        vector<pair<int, int>> bucket;
+        vector<int> res;
+        int n=0;
         for(int &n : nums) {
             count[n]++;
         }
-
-        vector<pair<int, int>> freq;
-        for(auto &c: count) {
-            freq.push_back({c.second, c.first});
+        
+        for(auto &c : count) {
+            bucket.push_back({c.second, c.first});
         }
 
-        sort(freq.begin(), freq.end());
+        sort(bucket.begin(), bucket.end());
 
-        int indx = 0;
-        vector<int> res;
-
-        for(int i = freq.size()-1; i >=0 ; i--) {
-            res.push_back(freq[i].second);
-            if(++indx == k) break;
+        for(int i = bucket.size()-1; i >=0; i--) {
+            res.push_back(bucket[i].second);
+            if(++n == k) break;
         }
+
         return res;
     }
 };
