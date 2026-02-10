8d5c4f853d5f0f636096083f44ea5e8874fc7e2c
ssougata-doolai <sougatadolai00@gmail.com>
2026-02-10 16:16:44 +0530

    Time: 160 ms (5.03%) | Memory: 37.5 MB (5.13%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,19 +1,47 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mpp;
-        vector<vector<string>> res;
+        // unordered_map<string, vector<string>> mpp;
+        // vector<vector<string>> res;
 
-        for(string &s : strs) {
-            string sortedS = s;
-            sort(sortedS.begin(), sortedS.end());
-            mpp[sortedS].push_back(s);
+        // for(string &s : strs) {
+        //     string sortedS = s;
+        //     sort(sortedS.begin(), sortedS.end());
+        //     mpp[sortedS].push_back(s);
+        // }
+
+        // for(auto pair : mpp) {
+        //     res.push_back(pair.second);
+        // }
+
+        // return res;
+
+        unordered_map<string, vector<string>> mpp;
+        vector<vector<string>> result;
+
+        for(string s : strs) {
+            vector<int> count(26, 0);
+            for(char c : s) {
+                count[c - 'a'] += 1;
+            }
+
+            string key = "";
+            for(int i : count) {
+                key += to_string(i) + "#";
+            }
+
+            mpp[key].push_back(s);
         }
 
         for(auto pair : mpp) {
-            res.push_back(pair.second);
+            cout << pair.first << " : ";
+            for (const auto &word : pair.second) {
+                cout << word << " ";
+            }
+            cout << endl;
+            result.push_back(pair.second);
         }
 
-        return res;
+        return result;
     }
 };
