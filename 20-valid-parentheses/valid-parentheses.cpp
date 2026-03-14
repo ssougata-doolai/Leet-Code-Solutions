eeb6fa43de7996467777a8a5574cbd47f4e827a0
ssougata-doolai <sougatadolai00@gmail.com>
2026-03-14 11:56:07 +0530

    Time: 2 ms (15.53%) | Memory: 8.9 MB (38.52%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int counter1 = 0, counter2 = 0, counter3 = 0;
-        stack<char> st;
-
+        stack<char> paren;
         for(char c : s) {
             if(c == '(' || c == '{' || c == '[') {
-                st.push(c);
-            } else {
-                if(st.empty()) return false;
-                
-                char top = st.top();
-                st.pop();
-                if((c == ')' && top != '(') ||
-                   (c == '}' && top != '{') ||
-                   (c == ']' && top != '[')
-                ) return false;
+                paren.push(c);
+            }
+            else {
+                if(paren.empty()) return false;
+                if(c == ')' && paren.top() == '(' || 
+                    c == '}' && paren.top() == '{' ||
+                    c == ']' && paren.top() == '['
+                    ) {
+                    paren.pop();
+                }
+                else {
+                    return false;
+                }
             }
         }
-
-        return st.empty();
+        return paren.empty();
     }
 };
