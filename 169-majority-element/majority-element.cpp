6455219d10fe761449ebcaf13d550c0eb9e4edff
ssougata-doolai <sougatadolai00@gmail.com>
2026-04-08 00:30:33 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (63.37%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int res, count=0;
+        for(int num : nums) {
+            if(count == 0) {
+                res = num;
+            } 
+            count += (res == num) ? 1 : -1;
+        }
+        return res;
+    }
+};
