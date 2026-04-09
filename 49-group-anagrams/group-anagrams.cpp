2427016eea60a888526b9bf23f4e846d28e6847e
ssougata-doolai <sougatadolai00@gmail.com>
2026-04-09 22:32:31 +0530

    Time: 74 ms (7.65%) | Memory: 34.3 MB (11.43%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -4,9 +4,17 @@ public:
         unordered_map<string, vector<string>> mpp;
         vector<vector<string>> res;
 
-        for(auto s : strs) {
-            string s1 = s;
-            sort(s1.begin(), s1.end());
+        for(auto &s : strs) {
+            vector<int> count(26, 0);
+            for(char c : s) {
+                count[c - 'a'] += 1;
+            }
+            
+            string s1 = to_string(count[0]);
+            for(int i = 1; i < 26; i++) {
+                s1 += ',' + to_string(count[i]);
+            }
+            // cout<< s <<" : " << s1 << endl;
             mpp[s1].push_back(s);
         }
         for(auto &item : mpp) {
