7e67c5384d3ae704e1264dacf659c025578159ee
ssougata-doolai <sougatadolai00@gmail.com>
2026-04-09 22:12:47 +0530

    Time: 19 ms (45.52%) | Memory: 24.9 MB (87.07%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,47 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // unordered_map<string, vector<string>> mpp;
-        // vector<vector<string>> res;
-
-        // for(string &s : strs) {
-        //     string sortedS = s;
-        //     sort(sortedS.begin(), sortedS.end());
-        //     mpp[sortedS].push_back(s);
-        // }
-
-        // for(auto pair : mpp) {
-        //     res.push_back(pair.second);
-        // }
-
-        // return res;
-
         unordered_map<string, vector<string>> mpp;
-        vector<vector<string>> result;
+        vector<vector<string>> res;
 
-        for(string s : strs) {
-            vector<int> count(26, 0);
-            for(char c : s) {
-                count[c - 'a'] += 1;
-            }
-
-            string key = "";
-            for(int i : count) {
-                key += to_string(i) + "#";
-            }
-
-            mpp[key].push_back(s);
+        for(auto s : strs) {
+            string s1 = s;
+            sort(s1.begin(), s1.end());
+            mpp[s1].push_back(s);
         }
-
-        for(auto pair : mpp) {
-            cout << pair.first << " : ";
-            for (const auto &word : pair.second) {
-                cout << word << " ";
-            }
-            cout << endl;
-            result.push_back(pair.second);
+        for(auto &item : mpp) {
+            res.push_back(item.second);
         }
-
-        return result;
+        return res;
     }
 };
