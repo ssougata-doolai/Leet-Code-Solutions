c86cefee4430c37ed5c2f99319f80c7eb3ca2578
ssougata-doolai <sougatadolai00@gmail.com>
2026-04-13 18:08:34 +0530

    Time: 79 ms (67.71%) | Memory: 88.9 MB (65.99%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        // sort(nums.begin(), nums.end());
-        // int max_counter = 1, counter = 1;
-        // if(nums.size() == 0) return 0;
-        // for(int i = 1; i < nums.size(); i++) {
-        //     if(nums[i-1] == nums[i]) continue;
-        //     if(nums[i-1] - nums[i] == -1) {
-        //         counter += 1;
-        //         max_counter = max(max_counter, counter);
-        //         continue;
-        //     }
-        //     counter = 1;
-        // }
+        if(nums.size() == 0) return 0;
 
-        // return max_counter;
-
-        if(nums.size() == 0)  return 0;
-
-        unordered_set<int> s;
-        for(int i = 0; i < nums.size(); i++) {
-            s.insert(nums[i]);
+        int count, max_count = 0;
+        unordered_set<int> seen;
+        for(int i : nums) {
+            seen.insert(i);
         }
-
-        int max_count = 0, count = 0;
-        for(int item : s) {
-            if(s.count(item-1) == 0) {
-                while(s.count(item++) != 0) {
-                    count += 1;
-                    max_count = max(max_count, count);
-                };
-                count = 0;
-            } 
+        for(int n : seen) {
+            if(seen.find(n - 1) == seen.end()) {
+                count = 1;
+                while(seen.find(++n) != seen.end()) count++;
+                max_count = max(max_count, count);
+            }
+            
         }
         return max_count;
     }
