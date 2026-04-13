b3b148fadbacae25019e881f8745ea5433067bb9
ssougata-doolai <sougatadolai00@gmail.com>
2026-04-13 18:35:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (93.86%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i = 0, j = s.size() - 1;
+        string newS;
+        int i = 0, j = s.size()-1;
         while(i < j) {
-            if(!isalnum(s[i])) {
-                i++;
-                continue;
-            }
-
-            if(!isalnum(s[j])) {
-                j--;
-                continue;
-            }
-
+            while(!isalnum(s[i]) && i < j) i++;
+            while(!isalnum(s[j]) && i < j) j--;
             if(tolower(s[i]) != tolower(s[j])) return false;
             i++;
             j--;
