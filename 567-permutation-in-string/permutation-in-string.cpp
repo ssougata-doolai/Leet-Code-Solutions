b30f3cbc12425f6823b4b94394d30a25a9b14aa7
ssougata-doolai <sougatadolai00@gmail.com>
2026-04-19 14:03:17 +0530

    Time: 4 ms (51.89%) | Memory: 9.9 MB (69.16%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,18 +1,47 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int i, n = s2.size(), m = s1.size();
-        string subs;
-        sort(s1.begin(), s1.end());
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        
+        vector<int> freq1(26, 0), freq2(26, 0);
+        int l = 0, r, matches = 0;
+        for(int i = 0; i < s1.size(); i++) {
+            freq1[s1[i] - 'a']++;
+            freq2[s2[i] - 'a']++;
+        }
 
-        for(i = 0; i < n; i++) {
-            subs = s2.substr(i, m);
-            sort(subs.begin(), subs.end());
-
-            if(s1 == subs) {
-                return true;
+        for(int i = 0; i < 26; i++) {
+            if(freq1[i] == freq2[i]) {
+                matches++;
             }
         }
-        return false;
+
+        for(r = s1.size(); r < s2.size(); r++) {
+            if(matches == 26) {
+                return true;
+            }
+
+            int indx1 = s2[r] - 'a';
+            freq2[indx1]++;
+            if(freq1[indx1] == freq2[indx1]) {
+                matches++;
+            } else if(freq1[indx1] + 1 == freq2[indx1]) {
+                matches--;
+            }
+
+            int indx2 = s2[l] - 'a';
+            freq2[indx2]--;
+            if(freq1[indx2] == freq2[indx2]) {
+                matches++;
+            } else if(freq1[indx2] - 1 == freq2[indx2]) {
+                matches--;
+            }
+            
+            l++;
+        }
+        
+        return matches == 26;
     }
 };
